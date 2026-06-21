32f32771b528985d7497b537b676b592b33f3275
Shay Mackintosh <shaymackintosh@Shays-iMac.local>
2026-06-21 17:22:48 +0100

    Update backend

--- helpers/index.js
+++ helpers/index.js
@@ -2,7 +2,7 @@ const sanitizeHtml = require('sanitize-html');
 const { data, saveData } = require('../data');
 const toxicity = require('@tensorflow-models/toxicity');
 
-const modelPromise = toxicity.load(0.9);
+const modelPromise = toxicity.load(0.5);
 
 // ---------------- TEXT NORMALIZER ----------------
 function normalizeText(text) {
