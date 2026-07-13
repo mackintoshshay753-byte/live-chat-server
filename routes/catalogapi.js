3542b4d0bfa9ea1cf6350aae588489939d6e9d0f
Shay Mackintosh <shaymackintosh@Shays-iMac.local>
2026-07-13 05:40:45 +0100

    fix backend

--- routes/catalogapi.js
+++ routes/catalogapi.js
@@ -4,6 +4,19 @@ const { data, saveData } = require('../data');
 
 const ALLOWED_UPLOAD_IDS = [1];
 
+// Get single outfit
+router.get('/:id', (req, res) => {
+  const outfit = data.outfitCatalog?.[req.params.id];
+  if (!outfit) return res.status(404).json({ success: false, message: "Outfit not found" });
+  
+  res.json({ success: true, outfit: {
+    id: outfit.id, name: outfit.name, price: outfit.price,
+    thumbnailUrl: outfit.thumbnail, head: outfit.head,
+    creator: outfit.uploadedBy, creatorUrl: `/users/profile?id=${outfit.uploadedBy}`
+  }});
+});
+
+
 // Get all outfits
 router.get('/', (req, res) => {
   if (!data.outfitCatalog) data.outfitCatalog = {};
