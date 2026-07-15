459c4084e4cb6c87c1b13210604199cc8246c254
Shay Mackintosh <shaymackintosh@Shays-iMac.local>
2026-07-15 02:00:12 +0100

    fix backend

--- data/catalogtable.js
+++ data/catalogtable.js
@@ -6,7 +6,7 @@ module.exports = {
   // Shop items start at ID 5
   5: { id: 5, name: "Classic Blue Shirt", price: 0, head: "/images/avatars/catalog/head/blue-shirt.png", thumbnail: "/images/avatars/catalog/thumbnail/blue-shirt-thumb.png", uploadedBy: 1, sales: 0, views: 0, category: 13, gender: "male" },
   6: { id: 6, name: "Classic Blue Shirt", price: 0, head: "/images/avatars/catalog/head/blue-shirt.png", thumbnail: "/images/avatars/catalog/thumbnail/blue-shirt-thumb.png", uploadedBy: 1, sales: 0, views: 0, category: 13, gender: "male" },
-  7: { id: 7, name: "Classic Blue Shirt", price: 0, head: "/images/avatars/catalog/head/blue-shirt.png", thumbnail: "/images/avatars/catalog/thumbnail/blue-shirt-thumb.png", uploadedBy: 1, sales: 0, views: 0, category: 13, gender: "male" },
+  7: { id: 7, name: "Dino Andrea", price: 0, head: "/images/avatars/catalog/head/dino-andrea.png", thumbnail: "/images/avatars/catalog/thumbnail/dino_andrea.png", uploadedBy: 1, sales: 0, views: 0, category: 13, gender: "male" },
   8: { id: 8, name: "Pastel Comfort", price: 0, head: "/images/avatars/catalog/head/girlnoob-1.png", thumbnail: "/images/avatars/catalog/thumbnail/girlnoob_1.png", uploadedBy: 1, sales: 0, views: 0, category: 13, gender: "female" },
   9: { id: 9, name: "Classic Blue Shirt", price: 0, head: "/images/avatars/catalog/head/blue-shirt.png", thumbnail: "/images/avatars/catalog/thumbnail/blue-shirt-thumb.png", uploadedBy: 1, sales: 0, views: 0, category: 13, gender: "female" },
   10: { id: 10, name: "Classic Blue Shirt", price: 0, head: "/images/avatars/catalog/head/blue-shirt.png", thumbnail: "/images/avatars/catalog/thumbnail/blue-shirt-thumb.png", uploadedBy: 1, sales: 0, views: 0, category: 13, gender: "female" },
